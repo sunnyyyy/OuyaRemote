2155d00874d2b3a3c29bcb0c03fae826879854d0
yliao001 <sunnylaio123@gmail.com>
2025-12-03 00:22:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ let textTD = document.querySelector('.sendTD') ;
 let videoTD = document.querySelector('.controlvideo') ;
 let videospeedTD = document.querySelector('.videospeed') ;
 
+const value = document.querySelector("#value");
+const input = document.querySelector("#speed_input");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+  value.textContent = event.target.value;
+});
+
 let state = { 
   "sliderOne": 0,
   "juliaSays": "hello!",
